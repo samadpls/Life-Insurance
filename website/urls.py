ab4969c94db68eddca578cce247fac50c01cf103
Muhammad-Shaheer-khan <shaheerk2233@gmail.com>
2023-01-12 00:42:45 +0500

    add notification, RiskAssessment and their pages

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('form',form,name='form')
+    path('form',form,name='form'),
+    path('notificatin',notification,name='Risk Assessment notification page'),
+    path('RiskAssessment',RiskAssessment,name='Risk Assessment')
 ]
--- website/views.py
+++ website/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import redirect, render
 def home(request):
     return render(request, 'index.html',{})
 def form(request):
-    return render(request, 'form.html',{})    
+    return render(request, 'form.html',{})  
+
+def notification(request):
+    return render(request, 'Notification.html',{})   
+
+def RiskAssessment(request):
+    return render(request, 'RiskAssessment.html',{})    
