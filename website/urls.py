7b730767a99fcb7ca6ef13165a0dee17c1b61925
Muhammad-Shaheer-khan <shaheerk2233@gmail.com>
2023-01-16 00:16:20 +0500

    update notification and risk Assesment pg

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('form',form,name='form'),
-    path('notificatin',notification,name='Risk Assessment notification page'),
-    path('RiskAssessment',RiskAssessment,name='Risk Assessment')
+    path('notification',notification,name='Risk Assessment notification page'),
+    path('RiskAssessment',RiskAssessment,name='RiskAssessment')
 ]
