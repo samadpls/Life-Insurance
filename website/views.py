1b646b439d982f22eda04c280ace0e0b9482bf68
samadpls <abdulsamadsid1@gmail.com>
2023-01-20 15:30:11 +0500

    working

--- website/views.py
+++ website/views.py
@@ -4,6 +4,9 @@ from registration.models import *
 # from Signup.models import Signup
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+import json
+from django.core.mail import send_mail
 
 
 
@@ -13,9 +16,58 @@ def home(request):
 
     return render(request, 'index.html', {})
 
-    return render(request, 'index.html',{})
+otp=""
+@csrf_exempt
+def send_otp(request):
+    global otp
+    if request.method=="POST":
+        import random
+        import smtplib
+        import string
+        from email.mime.text import MIMEText
+
+        # generate a random 5-letter OTP
+        otp = ''.join(random.choices(string.digits, k=5))
+
+        # create message
+        data = json.loads(request.body)
+        email = data['email']
+        from_email = "sajidahsan67@gmail.com"
+        password = "btjvcelmsuhxdmsz"
+        subject = "State Life OTP"
+        body = f"Thank you for choosing our service.\n\n Your OTP is {otp}.\n\n Please enter this OTP in the verification field to complete the process.\n\nPlease note that this OTP is valid for a limited time and will expire after one time\n\n.If you did not request this OTP or if you have any concerns, please contact our support team immediately.\nThank you for choosing our service.\nBest regards,\nState life company"
+
+        try:
+            send_mail(subject, body, from_email, [email], fail_silently=False)
+            return JsonResponse({'status': 'success'})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+@csrf_exempt
+def checkOTP(request):
+
+    global otp
+    if request.method=="POST":
+            data = json.loads(request.body)
+            print(data['otp'])
+            print(otp,"global")
+            if str(otp)==str(data['otp']):
+                return JsonResponse({'status': 'success', 'message': "Re-enter the OTP"})
+            
+            
+            else:
+                try:
+                    return JsonResponse({'status': 'error', 'message': "Re-enter the OTP"})
+                except Exception as e:
+                    return JsonResponse({'status': 'error', 'message': str(e)})
+                
+
 def form(request):
     return render(request, 'form.html',{})  
+
+def payPremium(request):
+    return render(request, 'payPremium.html',{})        
 
 def notification(request):
     
