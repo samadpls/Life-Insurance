a5e99dc2c51678365dc7a165823ab90ed3d75079
samadpls <abdulsamadsid1@gmail.com>
2023-01-21 20:30:13 +0500

    working hard

--- website/urls.py
+++ website/urls.py
@@ -4,19 +4,18 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('form',form,name='form'),
-    path('notification',notification,name='Risk Assessment notification page'),
-    path('RiskAssessment',RiskAssessment,name='RiskAssessment'),
-    path("target",target, name="target"),
-    path('delete /<str:team_id>/',delete_item,name="delete"),
+    path('form', form, name='form'),
+    path('notification', notification, name='Risk Assessment notification page'),
+    path('RiskAssessment', RiskAssessment, name='RiskAssessment'),
+    path("target", target, name="target"),
+    path('delete /<str:team_id>/', delete_item, name="delete"),
     path('send-otp', send_otp, name='send_otp'),
-    path('checkOTP', checkOTP, name='checkOTP'),
-    path('payPremium',payPremium,name='payPremium'),
-    path('login',login,name='login'),
-    path("jevansaathi",jevansaathi,name='jevansaathi'),
-    path("WholeLifeAssurance",WholeLifeAssurance,name='WholeLifeAssurance'),
-    path("loggedin",onlogin,name="loggedin"),
-    path("Sadabahar",Sadabahar,name='Sadabahar'),
-    path("ChildProtection",ChildProtection,name='ChildProtection')
+    path('payPremium', payPremium, name='payPremium'),
+    path('login', login, name='login'),
+    path("jevansaathi", jevansaathi, name='jevansaathi'),
+    path("WholeLifeAssurance", WholeLifeAssurance, name='WholeLifeAssurance'),
+    path("loggedin", onlogin, name="loggedin"),
+    path("Sadabahar", Sadabahar, name='Sadabahar'),
+    path("ChildProtection", ChildProtection, name='ChildProtection')
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,26 +1,87 @@
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from registration.models import *
-# from Signup.models import Signup
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 import json
+import uuid
 from django.core.mail import send_mail
 
 
-
 # Create your views here.
+login_id = ""
+@csrf_exempt
+def login(request):
+    global login_id
+    if request.method == 'GET':
+        try:
+            login_email = request.GET.get("login_name")
+            login_pwd = request.GET.get("login_pwd")
+            if "@teams.com" in login_email:
+                try:
+                    team = team_register.objects.get(
+                        team_email=login_email, team_password=login_pwd)
+                    return redirect("target")
+                except team_register.DoesNotExist:
+                    messages.error(request, "Email or password is not correct")
+                    return redirect('login')
+            elif "@salesmanager.com" in login_email:
+                pass
+
+            else:
+                try:
+                    client = register.objects.get(
+                        user_name=login_email, user_password=login_pwd)
+                    login_id = client.user_id
+                    print(login_id, "index mai bhij raha ho")
+                    # login wala page
+                    return render(request, 'indexlogin.html', {})
+                except register.DoesNotExist:
+                    messages.error(request, "Email or password is not correct")
+                    return redirect('login')
+
+        except:
+            return render(request, "login.html")
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        pwd = request.POST.get('password')
+        if not (("@teams.com" or "@salesmanager.com") in email):
+            cpwd = request.POST.get('cpassword')
+            reg_id = register.objects.all()
+            for id in reg_id:
+                if id.user_name == email:
+                    messages.error(request, "Email already exists")
+                    return redirect('login')
+
+            if pwd == cpwd:
+                data = register()
+                data.user_name = email
+                data.user_password = pwd
+                data.save()
+
+            else:
+                messages.error(request, "Password doesn't match")
+                return redirect('login')
+
+        else:
+            messages.error(request, "Invalid Email")
+            return redirect('login')
+
+    return render(request, 'login.html', {})
+
 
 def home(request):
 
     return render(request, 'index.html', {})
 
-otp=""
+
+otp = ""
 @csrf_exempt
 def send_otp(request):
     global otp
-    if request.method=="POST":
+    if request.method == "POST":
         import random
         import smtplib
         import string
@@ -44,116 +105,117 @@ def send_otp(request):
             return JsonResponse({'status': 'error', 'message': str(e)})
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-@csrf_exempt
-def checkOTP(request):
+# @csrf_exempt
+# def checkOTP(request):
 
-    global otp
-    if request.method=="POST":
-            data = json.loads(request.body)
-            print(data['otp'])
-            print(otp,"global")
-            if str(otp)==str(data['otp']):
-                form(data)
-                return JsonResponse({'status': 'success', 'message': "Re-enter the OTP"})
-            
-            
-            else:
-                try:
-                    return JsonResponse({'status': 'error', 'message': "Re-enter the OTP"})
-                except Exception as e:
-                    return JsonResponse({'status': 'error', 'message': str(e)})
-                
+#     global otp
+#     if request.method=="POST":
+#             data = json.loads(request.body)
+#             print(data['otp'])
+#             print(otp,"global")
+#             if str(otp)==str(data['otp']):
+#                 form(data)
+#                 return JsonResponse({'status': 'success', 'message': "Re-enter the OTP"})
+
+
+#             else:
+#                 try:
+#                     return JsonResponse({'status': 'error', 'message': "Re-enter the OTP"})
+#                 except Exception as e:
+#                     return JsonResponse({'status': 'error', 'message': str(e)})
+
 @csrf_exempt
 def form(request):
-    
     global otp
-    if request.method=="POST":
-            data = json.loads(request.body)
-            
-            print(data['otp'])
-            print(otp,"global")
-            form_class=Form()
-            print(data)
-            if str(otp)==str(data['otp']):
-                name=data['name']
-                fname=data['fname']
-                phone=data['phone']
-                email=data['email']
-                address=data['address']
-                dob=data['dob']
-                age=data['age']
-                cities=data['cities']
-                nominee=data['nominee']
-                dobnominee=data['dobnominee']
-                agenominee=data['agenominee']
-                nomineeRelation=data['nomineeRelation']
-                mincome=data['mincome']
-                suminsured=data['suminsured']
-                term=data['term']
-                # operation=data['operation']
-                # premium=data['premium']
-                
-                
-                form_class.name=name
-                form_class.father_name=fname
-                form_class.phone=phone
-                form_class.email=email
-                form_class.address=address
-                form_class.dob=dob
-                form_class.age=age
-                form_class.cities=cities
-                form_class.nominee=nominee
-                form_class.dob_nominee=dobnominee
-                form_class.age_nominee=agenominee
-                form_class.nominee_relation=nomineeRelation
-                form_class.mincome=mincome
-                form_class.suminsured=suminsured
-                form_class.term=term
-                # form_class.operation=operation
-                # form_class.premium=premium
-                
-                
-                
-                
-                # form_class.dob='2020-03-02'
-                
-                print(name,fname)
-                form_class.save()
-                return JsonResponse({'status': 'success', 'message': "Re-enter the OTP"})
-            
-            
-            else:
-                try:
-                    return JsonResponse({'status': 'error', 'message': "Re-enter the OTP"})
-                except Exception as e:
-                    return JsonResponse({'status': 'error', 'message': str(e)})
-       
-    
-        
-        
-    return render(request, 'form.html',{})  
+    global login_id
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        print(data['otp'])
+        print(otp, "global")
+        form_class = Form()
+        print(data)
+        if str(otp) == str(data['otp']):
+            print(login_id, "POST mai ha")
+            name = data['name']
+            fname = data['fname']
+            phone = data['phone']
+            email = data['email']
+            address = data['address']
+            dob = data['dob']
+            age = data['age']
+            cities = data['cities']
+            nominee = data['nominee']
+            dobnominee = data['dobnominee']
+            agenominee = data['agenominee']
+            nomineeRelation = data['nomineeRelation']
+            mincome = data['mincome']
+            suminsured = data['suminsured']
+            term = data['term']
+            # operation=data['operation']
+            # premium=data['premium']
+
+            form_class.login_id = str(login_id)
+            form_class.name = name
+            form_class.father_name = fname
+            form_class.phone = phone
+            form_class.email = email
+            form_class.address = address
+            form_class.dob = dob
+            form_class.age = age
+            form_class.cities = cities
+            form_class.nominee = nominee
+            form_class.dob_nominee = dobnominee
+            form_class.age_nominee = agenominee
+            form_class.nominee_relation = nomineeRelation
+            form_class.mincome = mincome
+            form_class.suminsured = suminsured
+            form_class.term = term
+            # form_class.operation=operation
+            # form_class.premium=premium
+
+            # form_class.dob='2020-03-02'
+
+            print(name, fname)
+            form_class.save()
+            return JsonResponse({'status': 'success', 'message': "Re-enter the OTP"})
+
+        else:
+            try:
+                return JsonResponse({'status': 'error', 'message': "Re-enter the OTP"})
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': str(e)})
+    else:
+        print("login_id", login_id)
+        agents=team_register.objects.all()
+        return render(request, 'form.html', {"agents":agents})
+
 
 def payPremium(request):
-    return render(request, 'payPremium.html',{})        
+    return render(request, 'payPremium.html', {})
+
 
 def notification(request):
-    
-    clients=['Jhon',"ali","umer","qasim"]
-    color=["alert-primary","alert-success","alert-info","alert-warning","alert-danger","alert-success","alert-secondary"]
 
-    
-    return render(request, 'Notification.html',{"clients":clients,"color":color})   
+    clients = ['Jhon', "ali", "umer", "qasim"]
+    color = ["alert-primary", "alert-success", "alert-info",
+             "alert-warning", "alert-danger", "alert-success", "alert-secondary"]
+
+    return render(request, 'Notification.html', {"clients": clients, "color": color})
+
 
 def RiskAssessment(request):
-    return render(request, 'RiskAssessment.html',{})  
+    return render(request, 'RiskAssessment.html', {})
 
-csrf_exempt
-def delete_item(request,team_id):
-    
-    data=team_register.objects.get(team_id=team_id)
+
+@csrf_exempt
+def delete_item(request, team_id):
+
+    data = team_register.objects.get(team_id=team_id)
     data.delete()
 
     return redirect('/target')
+
 
 @csrf_exempt
 def target(request):
@@ -162,91 +224,34 @@ def target(request):
         if "@teams.com" in team_email:
             team_name = request.POST.get('teamname')
             team_pas = request.POST.get('teampass')
-            if len(team_pas)>=5:
-                created=team_register.objects.filter(team_email=team_email).exists()
+            if len(team_pas) >= 5:
+                created = team_register.objects.filter(
+                    team_email=team_email).exists()
                 if created:
-                    messages.error(request,"Email already exists!")
+                    messages.error(request, "Email already exists!")
                     return redirect('target')
                 else:
-                    teams=team_register()
-                    teams.team_name=team_name
-                    teams.team_email=team_email
-                    teams.team_password=team_pas
+                    teams = team_register()
+                    teams.team_name = team_name
+                    teams.team_email = team_email
+                    teams.team_password = team_pas
                     teams.save()
-                    messages.error(request,"Team email already exist registered")
+                    messages.error(
+                        request, "Team email already exist registered")
                     return redirect('target')
             else:
-                messages.error(request,"length of password is not matched")
+                messages.error(request, "length of password is not matched")
                 return redirect('target')
         else:
-            messages.error(request,"team email is not valid")
-            return redirect('target')    
-        
-    task=team_register.objects.all()
-    
+            messages.error(request, "team email is not valid")
+            return redirect('target')
+
+    task = team_register.objects.all()
+
     print(task)
-        # task2={'task':task}
-        
-    return render(request,"target.html",{'task':task})
+    # task2={'task':task}
 
-
-
-            
-             
-@csrf_exempt
-def login(request):
-    
-    if request.method=='GET':
-        try:
-            login_email=request.GET.get("login_name")
-            login_pwd=request.GET.get("login_pwd")
-            if "@teams.com" in login_email:
-                try:
-                    team = team_register.objects.get(team_email=login_email,team_password=login_pwd)
-                    return redirect("target")
-                except team_register.DoesNotExist:
-                    messages.error(request,"Email or password is not correct")
-                    return redirect('login')  
-            elif "@salesmanager.com" in login_email:
-                pass
-             
-            else:
-                try:
-                    client = register.objects.get(user_name=login_email,user_password=login_pwd)
-                    return redirect("loggedin")
-                except register.DoesNotExist:
-                    messages.error(request,"Email or password is not correct")
-                    return redirect('login')  
-                 
-        except:
-            return render(request,"login.html")
-
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        pwd = request.POST.get('password')
-        if not( ("@teams.com" or "@salesmanager.com") in email):
-            cpwd = request.POST.get('cpassword')
-            reg_id=register.objects.all()
-            for id in reg_id:
-                if id.user_name==email:
-                    messages.error(request,"Email already exists")
-                    return redirect('login') 
-
-            if pwd == cpwd:
-                data = register()
-                data.user_name = email
-                data.user_password = pwd
-                data.save()
-                        
-            else:
-                messages.error(request,"Password doesn't match")
-                return redirect('login')
-               
-        else:
-            messages.error(request,"Invalid Email")
-            return redirect('login') 
-    
-    return render(request, 'login.html', {})
+    return render(request, "target.html", {'task': task})
 
 
 def jevansaathi(request):
@@ -263,6 +268,7 @@ def Sadabahar(request):
 
 def ChildProtection(request):
     return render(request, 'ChildProtection.html', {})
+
 
 def onlogin(request):
     return render(request, 'indexlogin.html', {})
