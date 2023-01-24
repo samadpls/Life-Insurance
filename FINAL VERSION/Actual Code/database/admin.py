52ce4b79c6a5d492bf4a01e397c444f36edc1ce2
samadpls <abdulsamadsid1@gmail.com>
2023-01-24 23:25:58 +0500

    done

---
+++ FINAL VERSION/Actual Code/database/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+    
+admin.site.register(register)
+admin.site.register(team_register)
+admin.site.register(Form)
+admin.site.register(TeamProgress)
+admin.site.register(payPrem)
+
+# Register your models here.
